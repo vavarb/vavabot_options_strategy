4fae5755e8c038c37ff31391a28b2a84e32cea70
Vagner <vavarb@yahoo.com>
2022-11-16 22:47:55 -0300

    Renamed file

--- pyinstaller.py
+++ pyinstaller.py
@@ -1,7 +1,7 @@
 import PyInstaller.__main__
 
 PyInstaller.__main__.run([
-    'vavabot_options_strategy_7_5_1.py',
+    'vavabot_options_strategy_7_7.py',
     '--onefile',
     '--noconsole',
     '--key=karenarthur12345',
